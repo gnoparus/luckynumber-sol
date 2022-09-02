35bb456060422e93a6a5b513e1a31ceedba82d32
gnoparus <surapong@gmail.com>
2022-09-02 14:17:09 +0700

    greater to greater equal

--- 5_LuckyNumber5.sol
+++ 5_LuckyNumber5.sol
@@ -2,28 +2,26 @@
 pragma solidity ^0.8.0;
 
 contract LuckyNumber5 {
-
     address public immutable owner;
     // uint256 public myLuckyNumber = 42;
 
-    mapping (address => uint256) public luckyNumbers;
+    mapping(address => uint256) public luckyNumbers;
 
     constructor() {
         owner = msg.sender;
     }
 
     function setLuckyNumber(uint256 _luckyNumber) public payable {
-        require(msg.value > 1e18, "Need more eth");
+        require(msg.value >= 1e18, "Need more eth");
         luckyNumbers[msg.sender] = _luckyNumber;
     }
 
     function ethBalance() public view returns (uint256) {
         return address(this).balance;
-    }    
+    }
 
     function withdraw() public {
         require(msg.sender == owner, "Only owner");
         payable(msg.sender).transfer(ethBalance());
     }
 }
-
