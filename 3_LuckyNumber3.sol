d6712d7ac5444102e4dfc7f3d0d409fc62321ee9
gnoparus <surapong@gmail.com>
2022-09-02 14:38:59 +0700

    Add event

--- 3_LuckyNumber3.sol
+++ 3_LuckyNumber3.sol
@@ -2,15 +2,16 @@
 pragma solidity ^0.8.0;
 
 contract LuckyNumber3 {
-
     // address immutable owner;
     // uint256 public myLuckyNumber = 42;
 
-    mapping (address => uint256) public luckyNumbers;
+    mapping(address => uint256) public luckyNumbers;
+
+    event LuckyNumber3__LuckyNumberSet(address person, uint256 luckyNumber);
 
     function setLuckyNumber(uint256 _luckyNumber) public {
         luckyNumbers[msg.sender] = _luckyNumber;
+
+        emit LuckyNumber3__LuckyNumberSet(msg.sender, _luckyNumber);
     }
-
 }
-
--- 4_LuckyNumber4.sol
+++ 4_LuckyNumber4.sol
@@ -2,19 +2,22 @@
 pragma solidity ^0.8.0;
 
 contract LuckyNumber4 {
-
     // address immutable owner;
     // uint256 public myLuckyNumber = 42;
 
-    mapping (address => uint256) public luckyNumbers;
+    mapping(address => uint256) public luckyNumbers;
+
+    event LuckyNumber4__LuckyNumberSet(address person, uint256 luckyNumber);
 
     function setLuckyNumber(uint256 _luckyNumber) public payable {
         require(msg.value > 1e18, "Need more eth");
+
         luckyNumbers[msg.sender] = _luckyNumber;
+
+        emit LuckyNumber4__LuckyNumberSet(msg.sender, _luckyNumber);
     }
 
     function ethBalance() public view returns (uint256) {
         return address(this).balance;
-    }    
+    }
 }
-
