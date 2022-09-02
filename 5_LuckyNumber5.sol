94849a861a87387fdf45cd1715c705e4a96f8c3a
gnoparus <surapong@gmail.com>
2022-09-02 14:35:15 +0700

    Introduce error and event

--- 5_LuckyNumber5.sol
+++ 5_LuckyNumber5.sol
@@ -1,19 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+error LuckyNumber5__NeedMoreEth();
+
 contract LuckyNumber5 {
     address public immutable owner;
     // uint256 public myLuckyNumber = 42;
 
     mapping(address => uint256) public luckyNumbers;
 
+    event LuckyNumberSet(address person, uint256 luckyNumber);
+
     constructor() {
         owner = msg.sender;
     }
 
     function setLuckyNumber(uint256 _luckyNumber) public payable {
-        require(msg.value >= 1e18, "Need more eth");
+        if (msg.value < 1e18) {
+            revert LuckyNumber5__NeedMoreEth();
+        }
+
         luckyNumbers[msg.sender] = _luckyNumber;
+
+        emit LuckyNumberSet(msg.sender, _luckyNumber);
     }
 
     function ethBalance() public view returns (uint256) {
